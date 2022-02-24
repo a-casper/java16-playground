53e3d83a73fb5a25817baa107a1ed173a1c321d9
Anthony Casper <anthony.casper@galvanize.com>
2022-02-24 16:31:42 -0600

    Lesson on Java Basics - 2/24/2022

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,12 +1,51 @@
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //System.out.println("Hello World");
+        //data types
 
-        System.out.println(grading(90));
-        System.out.println(grading(80));
-        System.out.println(grading(70));
-        System.out.println(grading(60));
-        System.out.println(grading(59));
+        // byte, short, int, long, float, double
+        int x = 100;
+        long y = 1000000000000L;
+
+        float z = 100.567f;
+        double zz = 1234567.87643823433423d;
+
+        System.out.println(x);
+
+        //int + float = float
+        //int newX = x + z; error because x + z returns float
+        //System.out.println(x + z);
+
+        // char, boolean
+        char letter = 'a';
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+//        if(true) {
+//            System.out.println(false);
+//        }
+
+        int i = 1;
+
+
+        System.out.println("While Loop:");
+        while(i < 5) {
+            System.out.println(i);
+            i++;
+        }
+
+
+        System.out.println("For Loop:");
+        for(int j = 0; j <= 10; j += 2) {
+            System.out.println(j);
+        }
+
+//        System.out.println(grading(90));
+//        System.out.println(grading(80));
+//        System.out.println(grading(70));
+//        System.out.println(grading(60));
+//        System.out.println(grading(59));
 
     }
 
